2e60cd28e0573a02434720fc4b88bfcd39eb7b4c
Prince Buencamino <princebuencamino@gmail.com>
2025-08-15 23:28:33 +0800

    update locationCircle

--- app/src/main/java/com/ccslay/safecircles/User.kt
+++ app/src/main/java/com/ccslay/safecircles/User.kt
@@ -1,12 +1,14 @@
 package com.ccslay.safecircles
 
+import com.ccslay.safecircles.zone.LocationCircle
+
 data class User(
     var email: String = "",
     var phoneNumber: String = "",
     var name: String = "",
     var currentLocation: String = "",
-    //var homeCircle: LocationCircles,
-  //  var savedCircles: ArrayList<LocationCircles>,
+    var homeCircle: LocationCircle,
+    var savedCircles: ArrayList<LocationCircle>,
     var friends: ArrayList<User>
 )
 
